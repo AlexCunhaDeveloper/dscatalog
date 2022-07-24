779c71db5b1bf5c9d8512fff2b558f6ebc68c1f6
Alex Cunha <alexcunhadev@gmail.com>
2022-07-23 23:13:05 -0300

    atualizando pageable

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -13,6 +13,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -30,7 +31,7 @@ public class ProductService {
     private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAllPage(PageRequest pageRequest){
+    public Page<ProductDTO> findAllPage(Pageable pageRequest){
         Page<Product> list = repository.findAll(pageRequest);
         return list.map(x -> new ProductDTO(x));
     }
